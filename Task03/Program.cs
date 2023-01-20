80b69277316c33f9eb796a7d45142f8444d4faf5
Julia <julia.masaeva@gmail.com>
2023-01-21 02:01:27 +0600

    Диапазоны для координат

---
+++ Task03/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+//x, y - возможные координаты точек
+
+Console.WriteLine("Введите номер четверти: ");
+int frame = int.Parse(Console.ReadLine());
+if (frame == 1) Console.WriteLine("x >= 0, y >= 0");
+if (frame == 2) Console.WriteLine("x <= 0, y >= 0");
+if (frame == 3) Console.WriteLine("x <= 0, y <= 0");
+if (frame == 4) Console.WriteLine("x >= 0, y <= 0");
