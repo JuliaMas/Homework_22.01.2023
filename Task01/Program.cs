bd436e12695c4c695e7f15dd83ea4a0125641d86
Julia <julia.masaeva@gmail.com>
2023-01-21 01:31:10 +0600

    Является ли день недели выходным

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите номер дня недели: ");
+int daynumber = int.Parse(Console.ReadLine());
+
+if (daynumber >=1 && daynumber <=5)
+{
+    Console.WriteLine("Это будний день!");
+}
+ else
+{
+     Console.WriteLine("Это выходной день!");
+}
+
